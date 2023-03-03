11ddcdddef45f4e5acd5e39d245deb9c22d2518c
Alonzo PINEAU <APINEAU>
2023-03-03 14:07:22 +0100

    route : delete serie + all series

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Serie;
 use App\Form\SerieType;
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
@@ -11,26 +12,48 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin/series', name: '_addSeries')]
-    public function addSerie(Request $req,PersistenceManagerRegistry $doctrine): Response
+    #[Route('/admin/series/all', name: '_titreIdSeries')]
+    public function titreIdSerie(PersistenceManagerRegistry $doctrine): Response
     {
-        $serie= new Serie();
+        $series = $doctrine->getRepository(Serie::class)->findAll();
+        return $this->render('admin/lesSeries.html.twig', [
+            'LesSeries' => $series,
+        ]);
+    }
+
+    #[Route('/admin/series', name: '_addSeries')]
+    public function addSerie(Request $req, PersistenceManagerRegistry $doctrine): Response
+    {
+        $serie = new Serie();
         $form = $this->createForm(SerieType::class, $serie);
         $form->handleRequest($req);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $doctrine->getManager();
             $entityManager->persist($serie);
             $entityManager->flush();
-            return $this->redirectToRoute('app_serie');
+            return $this->redirectToRoute('_titreIdSeries');
         }
         return $this->render('admin/addSerie.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-    #[Route('/admin/series/{id}', name: '_editSeries')]
-    public function editSerie(Request $req,PersistenceManagerRegistry $doctrine,$id): Response
+
+    #[Route('/admin/series/{id}', name: 'deleteSeries', methods: ['DELETE'])]
+    public function deleteSerie(PersistenceManagerRegistry $doctrine, $id, Request $request): Response
     {
-        $serie= $doctrine->getRepository(Serie::class)->find($id);
+        $serie = $doctrine->getRepository(Serie::class)->find($id);
+        $entityManager = $doctrine->getManager();
+        if ($this->isCsrfTokenValid('delete_serie', $request->get('token'))) {
+            $entityManager->remove($serie);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('_titreIdSeries');
+    }
+
+    #[Route('/admin/series/{id}', name: '_editSeries')]
+    public function editSerie(Request $req, PersistenceManagerRegistry $doctrine, $id): Response
+    {
+        $serie = $doctrine->getRepository(Serie::class)->find($id);
         $form = $this->createForm(SerieType::class, $serie);
         $form->handleRequest($req);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -42,6 +65,5 @@ class AdminController extends AbstractController
         return $this->render('admin/editSeries.html.twig', [
             'form' => $form->createView(),
         ]);
-        
     }
 }
